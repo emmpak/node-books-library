abeeee0e6bb0db9e4f21d13d173622bdedfd015f
emmpak <eiliev05@yahoo.com>
2020-04-27 07:56:58 +0100

    Add debug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var chalk = require('chalk')
+var chalk = require('chalk');
+var debug = require('debug')('app');
 
 var app = express();
 
@@ -8,5 +9,5 @@ app.get('/', function(req, res){
 })
 
 app.listen(3000, function() {
-    console.log(`listening on port ${chalk.green('3000')}`);
+    debug(`listening on port ${chalk.green('3000')}`);
 });
