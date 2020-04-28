a45aee273c2b5d0f34f578298594ef0f9f7ef3df
emmpak <eiliev05@yahoo.com>
2020-04-28 16:39:48 +0100

    Login user and redirect to new /profile route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,7 +8,16 @@ function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
       debug(req.body);
-      res.json(req.body);
+      // passport initializes the login method
+      // passport serializes user in session, redirects & deserialize it
+      req.login((req.body), () => {
+        res.redirect('/auth/profile');
+      });
+    });
+  authRouter.route('/profile')
+    .get((req, res) => {
+      // user in session deserialized
+      res.json(req.user);
     });
   return authRouter;
 }
