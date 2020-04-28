b9eb56d9bd98d62d2a8372bb18bb6a0f7fb3bed4
emmpak <eiliev05@yahoo.com>
2020-04-28 17:57:48 +0100

    Add bookService

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookController');
 
-function bookController(nav) {
+function bookController(bookService, nav) {
   function getIndex(req, res) {
     const url = 'mongodb://localhost:27017';
     const dbName = 'libraryApp';
@@ -43,6 +43,9 @@ function bookController(nav) {
 
         const col = await db.collection('books');
         const book = await col.findOne({ _id: new ObjectID(id) });
+
+        // bookService returns a promise which is converted to use await
+        book.details = await bookService.getBookById(book.bookId);
         res.render(
           'bookView',
           {
